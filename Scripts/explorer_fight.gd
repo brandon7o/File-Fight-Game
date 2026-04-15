4bae85e2398f0cc9813365381f1c97772cdbe3ba
bogawa <bogawa@iu.edu>
2026-04-15 18:12:08 -0400

    4/15/26 6:12pm update

--- Scripts/explorer_fight.gd
+++ Scripts/explorer_fight.gd
@@ -9,21 +9,44 @@ extends Node2D
 @onready var propertiesID = 2
 @onready var fileTexture
 @onready var folderTexture
-@onready var popupMenuPlayer = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpace/PopupMenu
-@onready var AddPopup = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpace/PopupMenu/AddPopup
+@onready var popupMenuPlayer = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpacePlayer/PopupMenu
+@onready var AddPopup = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpacePlayer/PopupMenu/AddPopup
 #TODO make function so that we know which "folder" we are currently in at all times
-@onready var mainPlayerFolder = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpace/ItemList
-@onready var mainPlayerSpace = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpace
-@onready var currentFolder
+@onready var mainPlayerFolder = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpacePlayer/ItemList
+@onready var mainPlayerSpace = $VSeparator/PlayerEnvironment/VBoxContainer/FolderFileSpacePlayer
+@onready var currentFolder = mainPlayerFolder
+@onready var backButton = $VSeparator/PlayerEnvironment/VBoxContainer/HBoxContainer/BackButton
+@onready var forwardButton = $VSeparator/PlayerEnvironment/VBoxContainer/HBoxContainer/ForwardButton
 
-#START HERE
-#Add a way to sort the files/folders whenever one is added
+#START HERE: WHAT IS NEEDED FOR PRE-ALPHA
+# = TODO
+## = DOING
+##- = DOING, ON PAUSE
+### = DONE
+##Add a way to sort the files/folders whenever one is added
 #Add a way to drag files into folders
-#Add a function to make the variable "currentFolder" know and recognise the current folder,
-#replacing most variables of mainPlayerFolder/Space with this variable or close relatives
-#Add a back button functionality that goes back one folder and is unselectable in mainPlayerFolder
+#Add naming for folders and files when you add them, also f2 can rename files/folders
+###Add a function to make the variable "currentFolder" know and recognise the current folder,
+###replacing most variables of mainPlayerFolder/Space with this variable or close relatives
+##-Add a back button functionality that goes back one folder and is unselectable in mainPlayerFolder
 #Add a way to differentiate from items in itemlist (this will be done by adding a lot of empty
 #texture2D's with the correct file ID, giving them correct ID varibles as seen in the ready function
+#Move functions to different scripts into different nodes that makes it more organized
+#Add turn-based gameplay
+#Add simple enemy and give it custom-made "ai"
+#Add UI for player HP and enemy HP
+#Add win-loss screen
+
+#START HERE: WHAT ELSE IS NEEDED FOR ALPHA
+#Upgrade screen that gives player new files
+#Levels and exp for player, giving more actions per turn
+#More enemies, up to level 10
+#General balancing
+
+#START HERE: WHAT ELSE IS NEEDED FOR BETA
+#Polish
+#Infinate enemies
+#Seeded games to allow randomness in every run (in upgrades mainly)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -52,17 +75,17 @@ func _on_item_list_empty_clicked(at_position: Vector2, mouse_button_index: int) 
 		popupMenuPlayer.position = get_global_mouse_position()
 		popupMenuPlayer.reset_size()
 		popupMenuPlayer.visible = true
-		mainPlayerFolder.deselect_all()
+		currentFolder.deselect_all()
 	if mouse_button_index == MOUSE_BUTTON_LEFT:
 		#Deselect current item(s)
-		mainPlayerFolder.deselect_all()
+		currentFolder.deselect_all()
 		pass
 
 #Function on when the player clicks some item
 #TODO Differentiate from main folder to other folders
 func _on_item_list_item_clicked(index, at_position, mouse_button_index):
 	if mouse_button_index == MOUSE_BUTTON_RIGHT:
-		if mainPlayerFolder.get_item_icon(index) == fileTexture:
+		if currentFolder.get_item_icon(index) == fileTexture:
 			#Sets the popup menu, moves it to the mouse position, and makes it visible
 			popupMenuPlayer.clear()
 			popupMenuPlayer.add_item("Delete", deleteID)
@@ -70,16 +93,16 @@ func _on_item_list_item_clicked(index, at_position, mouse_button_index):
 			popupMenuPlayer.position = get_global_mouse_position()
 			popupMenuPlayer.reset_size()
 			popupMenuPlayer.visible = true
-			mainPlayerFolder.deselect_all()
-			mainPlayerFolder.select(index)
+			currentFolder.deselect_all()
+			currentFolder.select(index)
 		else:
 			popupMenuPlayer.clear()
 			popupMenuPlayer.add_item("Properties", propertiesID)
 			popupMenuPlayer.position = get_global_mouse_position()
 			popupMenuPlayer.reset_size()
 			popupMenuPlayer.visible = true
-			mainPlayerFolder.deselect_all()
-			mainPlayerFolder.select(index)
+			currentFolder.deselect_all()
+			currentFolder.select(index)
 	if mouse_button_index == MOUSE_BUTTON_LEFT:
 		pass
 	
@@ -93,12 +116,15 @@ func _on_item_list_item_clicked(index, at_position, mouse_button_index):
 func _on_item_list_item_activated(index):
 	var fold
 	#TODO replace with current folder
-	if mainPlayerFolder.get_item_icon(index) == folderTexture:
+	if currentFolder.get_item_icon(index) == folderTexture:
 		for nodes in mainPlayerSpace.get_children():
 			if nodes.name == mainPlayerFolder.get_item_text(index):
-				mainPlayerFolder.visible = false
+				currentFolder.visible = false
 				var testFold = get_node(get_path_to(nodes))
 				testFold.visible = true
+				currentFolder = testFold
+				if (currentFolder != mainPlayerFolder):
+					backButton.disabled = false
 				testFold.add_item("testing", fileTexture)
 				break
 				
@@ -106,7 +132,7 @@ func _on_item_list_item_activated(index):
 
 #TODO replace with adjustable one
 func _on_popup_menu_id_pressed(id):
-	var fold = mainPlayerFolder
+	var fold = currentFolder
 	#Currently adds just a "file" named testing
 	#TODO: Add popup menu to allow for adding a file (from list of files you have) or folder
 	#if id == addID:
@@ -138,23 +164,98 @@ func _on_add_popup_index_pressed(index):
 	#If folder, add folder
 	#TODO Differentiate from main folder to other folders
 	if addPopup.get_item_id(index) == folderID:
-		mainPlayerFolder.add_item("Folder", folderTexture)
+		currentFolder.add_item("Folder", folderTexture)
 		#mainPlayerSpace.add_child(ItemList.new())
 		var newFolder = mainPlayerSpace.get_child(mainPlayerFolder.get_index()).duplicate()
 		mainPlayerSpace.add_child(newFolder)
 		#mainPlayerSpace.get_child(mainPlayerFolder).duplicate()
 		#TODO in the rename function, make sure to change the name of the correct itemlist
-		#mainPlayerSpace.get_child(mainPlayerFolder.get_child_count()-1).visible = false
-		#mainPlayerSpace.get_child(mainPlayerFolder.get_child_count()-1).name = "Folder"
-		#mainPlayerSpace.get_child(mainPlayerFolder.get_child_count()-1).size = mainPlayerFolder.size
 		newFolder.visible = false
 		newFolder.name = "Folder"
 		newFolder.clear()
-		#mainPlayerSpace.get_node(get_path_to(get_child(mainPlayerFolder.get_child_count()-1)))
-		pass
+		sort_by_alphabetical()
 	#Adds file of correct name to directory
 	#TODO Differentiate from main folder to other folders
 	if addPopup.get_item_id(index) == fileID:
-		mainPlayerFolder.add_item(addPopup.get_item_text(index), fileTexture)
-		pass
+		currentFolder.add_item(addPopup.get_item_text(index), fileTexture)
+		sort_by_alphabetical()
+	#Sorts the files/folders by folders then files
+	
 	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	#TODO Add backing functionality
+	
+	#If we are now in the mainPlayerFolder, we disable the button
+	if currentFolder == mainPlayerFolder:
+		backButton.disabled = true
+	#else, if the folder is not the mainPlayerFolder, and the back button is
+	#also disabled, then we enable it
+	elif currentFolder != mainPlayerFolder and backButton.disabled == true:
+		backButton.disabled = false
+	pass
+
+
+func _on_forward_button_pressed() -> void:
+	#TODO Add forward functionality
+	
+	#TODO If we ar in the most recent folder, disable the button
+	pass
+
+
+#sorting functions based on what is needed
+#TODO using mainplayerfolder to see methdos, change to currfolder when done
+func sort_by_alphabetical():
+	#gets the array of items
+	var itemsArray = []
+	for i in range(currentFolder.item_count):
+		itemsArray.append({
+			"text": currentFolder.get_item_text(i),
+			"icon": currentFolder.get_item_icon(i),
+			"metadata": currentFolder.get_item_metadata(i)
+		})
+	#Sorts the array
+	itemsArray.sort_custom(sort_alphabetical_helper)
+	mainPlayerFolder.clear()
+	for item in itemsArray:
+		if item["text"] != "":
+			var index = mainPlayerFolder.add_item(item["text"], item["icon"])
+			mainPlayerFolder.set_item_metadata(index, item["metadata"])
+
+func sort_alphabetical_helper(a,b):
+	var a_is_folder = a["icon"] == folderTexture
+	var b_is_folder = b["icon"] == folderTexture
+	if a_is_folder != b_is_folder:
+		return a_is_folder
+	return a["text"].to_lower() < b["text"].to_lower()
+
+func sort_by_size():
+	#gets the array of items
+	var itemsArray = []
+	for i in range(mainPlayerFolder.item_count):
+		itemsArray.append({
+			"text": mainPlayerFolder.get_item_text(i),
+			"icon": mainPlayerFolder.get_item_icon(i),
+			"metadata": mainPlayerFolder.get_item_metadata(i)
+		})
+	#Sorts the array
+	
+
+func sort_size_helper():
+	pass
+
+func sort_by_last_edited():
+	#gets the array of items
+	var itemsArray = []
+	for i in range(mainPlayerFolder.item_count):
+		itemsArray.append({
+			"text": mainPlayerFolder.get_item_text(i),
+			"icon": mainPlayerFolder.get_item_icon(i),
+			"metadata": mainPlayerFolder.get_item_metadata(i)
+		})
+	#Sorts the array
+	
+
+func sort_last_edited_helper():
+	pass
